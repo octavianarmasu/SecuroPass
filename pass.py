557488b28a60b8fca44878f0eff7eaca24f47586
Armasu Octavian <120184802+octavianarmasu@users.noreply.github.com>
2024-02-23 21:05:09 +0200

    Added GUI

--- pass.py
+++ pass.py
@@ -1,4 +1,6 @@
 import getpass
+import tkinter as tk
+from tkinter import messagebox
 
 
 def readPass():
@@ -45,28 +47,58 @@ def passCheck(password):
 def passStrength(n):
     # printing the strength of the password
     if n == 1:
-        print("Very weak password!")
+        return "very weak password!"
     elif n == 2:
-        print("Weak password!")
+        return "weak password!"
     elif n == 3:
-        print("Decent password!")
+        return "decent password!"
     elif n == 4:
-        print("Strong password!")
+        return "strong password!"
     elif n == 5:
-        print("Very strong password!")
+        return "very strong password!"
 
 
-print("Your password should contain a lower case letter," +
-      "an upper case letter, a digit, a special character.")
-print("It should be at least 8 characters long.")
-password = readPass()
-password2 = reenterPass()
-while password != password2:
-    print("Passwords don't match. Please try again")
-    password = readPass()
-    password2 = reenterPass()
-requirementsMet = sum(value for value in passCheck(password).values())
-passStrength(requirementsMet)
-# password will not be stored :)
-password = ""
-password2 = ""
+def checkPasswords():
+    if password.get() == password2.get():
+        requirementsMet = sum(
+            value for value in passCheck(password.get()).values())
+        password_strength = passStrength(requirementsMet)
+        messagebox.showinfo("Password Check: ", password_strength)
+    else:
+        feedback_label.config(text="Passwords do not match!", fg="red")
+        password.delete(0, tk.END)
+        password2.delete(0, tk.END)
+
+
+app = tk.Tk()
+app.title("Password Checker App")
+app.geometry("400x200")
+app.config(bg="white")
+
+welcome_label = tk.Label(
+    app, text="Welcome to the Password Checker App", bg="white")
+welcome_label.pack(pady=(10, 5))
+
+instruction_label = tk.Label(app, text="Your password should contain a lower case letter,"
+                                       " an upper case letter, a digit, a special character.\n"
+                                       "It should be at least 8 characters long.", bg="white", justify=tk.LEFT)
+instruction_label.pack(pady=(5, 10))
+password_label = tk.Label(app, text="Enter Password:", bg="white")
+password_label.pack()
+
+password = tk.Entry(app, show="*")
+password.pack()
+
+reenter_password_label = tk.Label(app, text="Re-enter Password:", bg="white")
+reenter_password_label.pack()
+
+password2 = tk.Entry(app, show="*")
+password2.pack()
+
+feedback_label = tk.Label(app, text="", bg="white")
+feedback_label.pack(pady=(5, 0))
+
+check_button = tk.Button(app, text="Check Password", command=checkPasswords)
+check_button.pack(pady=(10, 0))
+
+app.mainloop()
